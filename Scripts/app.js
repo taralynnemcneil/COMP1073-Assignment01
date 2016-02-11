f21dda376fdd00e9f75323aa467b0ad08aeb47e9
taralynnemcneil <taralynnemcneil@gmail.com>
2016-02-10 19:27:45 -0500

    Home Page Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -22,7 +22,7 @@
         
         var firstParagraph;
         firstParagraph = document.getElementById("firstParagraph");
-        firstParagraph.innerHTML = "My New Paragraph Data";
+        firstParagraph.innerHTML = "Hello, My name is Tara McNeil and this is my portfolio site. To about me check out my about page. To see some of my graphic design and web development work check out the projects page. If you like what you see here you click the contact button below or the contact page tab at the top to get in touch with me.";
     };
     
     
