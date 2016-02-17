3a3ca6a81c4005625a91dce9a62d970e642f7c86
taralynnemcneil <taralynnemcneil@gmail.com>
2016-02-17 17:09:20 -0500

    commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -20,7 +20,8 @@
     // project one
     var replaceFirstProject = function () {
         var firstProject;
-        firstProject = document.getElementById("firstProject").innerHTML = "This project was to name and create a dieline for snowboarding goggles for adult males. I went with a simple layout with a flat flue color for a calm feeling and triangle shapes to create the mountains.";
+        firstProject = document.getElementById("firstProject");
+        firstProject.innerHTML = "This project was to name and create a dieline for snowboarding goggles for adult males. I went with a simple layout with a flat flue color for a calm feeling and triangle shapes to create the mountains.";
     };
     
     // project two
